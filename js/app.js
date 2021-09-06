fcb98e93be1402eb806475404c93f5839a98a875
brunEdo <edobruno.m@gmail.com>
2021-09-06 11:07:49 +0200

    js refactoring

--- js/app.js
+++ js/app.js
@@ -1,35 +1,32 @@
-// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
-// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
-// Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+// Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
+// I numeri non possono essere duplicati.
+// In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+// La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
+// Al termine della partita il software deve comunicare il punteggio.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
 
 // # MINED FIELD
 
-// ## VARIABLES
+// ## SHORTCUT VARIABLES
 const field = document.getElementById("field");
 const startBtn = document.getElementById("startBtn");
 const score = document.getElementById("score");
 const userRows = document.getElementById("rows");
 const userMines = document.getElementById("mines");
-let points = 0;
 
 // ## FUNCTIONS
 // 1. Ask the number of cells to the user
-// const userChoosenNumber = (max) => {
-//     let userNumber = parseInt(prompt(`Choose a number of rows,  ${max} is the maximum`));
-//     if ( !isNaN(userNumber) && userNumber >= 0 && userNumber <= max ) {
-//         console.log(`userNumber ${userNumber}`);
-//         return userNumber;
-//     } else {
-//         alert("Invalid number");
-//         return userChooseANumber(max);
-//     }
-// }
 const userChoosenNumber = (input, max) => {
     let userNumber = parseInt(input);
     if ( !isNaN(userNumber) && userNumber > 0 && userNumber <= max ) {
         return userNumber;
     } else {
-        score.innerHTML = "Invalid Input";
+        return false;
     }
 }
 
@@ -49,42 +46,17 @@ const generateMines = (max, minesNumber) => {
         mines.push(minesStock[randomNumber]);
         minesStock.splice(randomNumber, 1);
     }
-    console.log(`minesIndex ${mines}`);
+
     return mines;
 }
 
-// 3. Function inject a number of html cells in the document, some of which are mined
-// const createField = (rows, columns) => {
-//     let cells = rows * columns;
-//     for (let i = 0; i < cells; i++) {
-//         field.innerHTML += `<div class="cell mined"></div>`
-//     }
-// }
-const createMinedField = (field, rows, cols, mines) => {
-    // 3a. reset the field
-    field.innerHTML = "";
-    field.dataset.rows = rows;
+// 3. Create list to update data-proximity of the non mined cells based on their proximity with the mined ones
+const genarateProximityData = (rows, cols, mines) => {
+    // 3a. diclare the proximity object
+    let proximityData = {};
 
-    // 3b. number of cells
-    let cells = rows * cols;
-
-    // 3c. generate mined and not mined cells
-    for (let i = 0; i < cells; i++) {
-        if (mines.includes(i)) {
-            field.innerHTML += `<div class="cell mined">${i}</div>`;
-        } else {
-            field.innerHTML += `<div data-proximity="0" class="cell">${i}</div>`;
-        }
-    }
-
-    // 3d. update data-proximity of the non mined cells
-    // based on their proximity with the mined ones
-    let cellsList = field.childNodes;
-    console.log(`cellsList ${field.childNodes[1]}`)
+    // 3b. for each mine generate a proximityList
     for(let i = 0; i < mines.length; i++){
-
-        console.log(`current mine ${mines[i]}`);
-
         // boolean variable based on the conditions to verify mine position
         let inFirstRow = mines[i] / cols < 1;
         let inLastRow = mines[i] / cols >= rows - 1;
@@ -101,6 +73,7 @@ const createMinedField = (field, rows, cols, mines) => {
         let cellBottom = mines[i] + cols; 
         let cellBottomRight = mines[i] + cols + 1;
 
+        // list of cells near the mine
         let proximityList = [];
 
         // give back the proximityList based on mine position
@@ -134,60 +107,103 @@ const createMinedField = (field, rows, cols, mines) => {
 
         // if the mine in the last col   
         } else if ( inLastCol ) {
-            proximityList = [ cellRight, cellTop, cellTopRight, cellBottom, cellBottomRight ];
+            proximityList = [ cellLeft, cellTop, cellTopLeft, cellBottom, cellBottomLeft ];
 
         } else {
             proximityList = [ cellLeft, cellRight, cellTopLeft, cellTop, cellTopRight, cellBottomLeft, cellBottom, cellBottomRight ];
         }
         
-        // update data proximity of the cells in the proximityList
-        for (let i = 0; i < proximityList.length; i++) {
-            cellsList[proximityList[i]].dataset.proximity = parseInt(cellsList[proximityList[i]].dataset.proximity) + 1;
-        }
+        // for each mine, used as key, store the list of nearbyCells
+        proximityData[mines[i]] = proximityList;
     }
+    // proximityData = {mine index : list of nearby cells indexs}
+    return proximityData;
 }
 
-// 4. Reveil if you clicked a bomb or a point
-const digCell = (event) => {
-    let diggedCell = event.target;
+// ## MAIN SCRIPT
+let points, maximumScore;
 
-    // 4a. if you clicked a mine => endgame
+// ### START GAME
+// 4. Start the script and create field
+startBtn.addEventListener("click", () => {
+    // 4a. get field dimension from the user
+    let rows = userChoosenNumber((userRows.value), 10);
+    if (rows == false) {
+        score.innerHTML = "Invalid Input";
+    }
+    let cols = rows;
+
+    // 4b. generate Mines Index s
+    let minesMultiplier = 16 / 3;
+    let minesNumber = parseInt(userChoosenNumber(userMines.value, 3) * minesMultiplier);
+    let mines = generateMines(rows * cols, minesNumber);
+
+    // 4c. reset the field
+    field.innerHTML = "";
+    field.dataset.rows = rows;
+
+    // 4d. nject mined and not mined cells
+    let cells = rows * cols;
+    for (let i = 0; i < cells; i++) {
+        if (mines.includes(i)) {
+            field.innerHTML += `<div class="cell mined">${i}</div>`;
+        } else {
+            field.innerHTML += `<div data-proximity="0" class="cell">${i}</div>`;
+        }
+    }
+
+    // 4e. update data proximity of the cells in the proximityList
+    // proximityData = {mine index : list of nearby cells indexs}
+    let proximityData = genarateProximityData(rows, cols, mines);
+    for ( let mine in proximityData) {
+        for (let nearbyCell = 0; nearbyCell < proximityData[mine].length; nearbyCell++) {
+            field.childNodes[proximityData[mine][nearbyCell]].dataset.proximity = parseInt(field.childNodes[proximityData[mine][nearbyCell]].dataset.proximity) + 1;
+        }
+    }
+
+    // 4f. reset the score
+    points = 0;
+    maximumScore = (rows * cols) - minesNumber; 
+
+});
+
+// ### PLAY 
+// 5. Add event listener to the field and activete script for each cells clicked
+field.addEventListener("click", (event) => {
+    let diggedCell = event.target;
+    let endGame = "";
+
+    // 5a. if you clicked a mine => endgame
     if (diggedCell.classList.contains("mined")) {
         alert("BOOOOOOOOOM!!!");
-        // reset the grid
-        field.dataset.rows = 0;
-        // popup the results
-        field.innerHTML = field.innerHTML = `<div class="flex-center"><div class="banner"><h1 id="results">YOU LOST</h1></div></div>`;
+        endGame = "lose";
 
-    // 4b. if you click a point update the score
+    // 5b. if you click a point update the score
     } else if (!diggedCell.classList.contains("digged")) {
         points += 1;
         score.innerHTML = points;
     }
 
-    // 4c. add class digged to reveal cell content
+    // 5c. add class digged to reveal cell content
     diggedCell.classList.add("digged");
-}
 
-// ## EVENT LISTENERS
-// 5. Add event listener to the field
-field.addEventListener("click", digCell);
-
-// ## MAIN SCRIPT
-// 6. Start the script and create field
-startBtn.addEventListener("click", () => {
-
-    let rows = parseInt(userChoosenNumber((userRows.value), 10));
-    let cols = rows;
-
-    let minesNumber = parseInt(userChoosenNumber(userMines.value, 3) * rows);
-    createMinedField(field, rows, cols, generateMines(rows * cols, minesNumber));
-
-    // reset the score
-    points = 0;
-
-    if ( points == ((rows * cols) - minesNumber) ) {
-        field.dataset.rows = 0;
-        field.innerHTML = `<div class="flex-center"><div class="banner"><h1 id="results">YOU WON</h1></div></div>`;
+    // 5d. if user get the maximum score end the game
+    if ( points == maximumScore ) {
+        endGame = "won";
     }
-})
+
+
+// ### END GAME
+    // 6. if endGame is not a empty string end the game
+    if ( endGame ) {
+        field.dataset.rows = 0;
+        if ( endGame === "won" ) {
+            field.innerHTML = `<div class="flex-center"><div class="banner"><h1 id="results">YOU WON</h1></div></div>`;
+        }
+        if ( endGame === "lose" ) {
+            field.innerHTML = `<div class="flex-center"><div class="banner"><h1 id="results">YOU LOST</h1></div></div>`;
+        }
+        endGame = "";
+    }
+});
+
